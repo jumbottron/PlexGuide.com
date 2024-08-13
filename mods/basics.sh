b91992d667a4d510d4dbd1d2757598048cb08d7c
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2024-08-13 19:04:48 -0400

    Update basics.sh

--- mods/basics.sh
+++ mods/basics.sh
@@ -1,1 +1,21 @@
+!/bin/bash
 
+# Function to check and create symbolic links for plexguide and pg commands
+check_and_create_commands() {
+    if [[ ! -f "/usr/local/bin/plexguide" ]]; then
+        sudo ln -s /pg/scripts/menu.sh /usr/local/bin/plexguide
+        sudo chmod +x /usr/local/bin/plexguide
+    fi
+
+    if [[ ! -f "/usr/local/bin/pg" ]]; then
+        sudo ln -s /pg/scripts/menu.sh /usr/local/bin/pg
+        sudo chmod +x /usr/local/bin/pg
+    fi
+
+    # Apply chmod +x to every file in /pg/scripts/ and /pg/apps/
+    sudo chmod +x /pg/scripts/*
+    sudo chmod +x /pg/apps/*
+}
+
+# Run the check and create commands if necessary
+check_and_create_commands
