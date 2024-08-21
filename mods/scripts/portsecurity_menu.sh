b115f75b17b77d22f21692e5e34bd908d489eeb6
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2024-08-21 10:07:01 -0400

    Update portsecurity_menu.sh

--- mods/scripts/portsecurity_menu.sh
+++ mods/scripts/portsecurity_menu.sh
@@ -27,34 +27,6 @@ prompt_for_code() {
   done
 }
 
-# Function to view open ports
-view_open_ports() {
-  clear
-  echo -e "\e[31mPG: Port Security View\e[0m"
-  echo
-
-  # Replace with the actual command to list open ports and sort them numerically
-  open_ports=$(ss -tuln | grep LISTEN | awk '{print $5}' | cut -d: -f2 | sort -n | tr '\n' ',' | sed 's/,$//')
-
-  # Wrap open ports to fit within an 80-character width
-  width=80
-  current_line=""
-  for port in ${open_ports//,/ }; do
-    if [ ${#current_line} -eq 0 ]; then
-      current_line="$port"
-    elif [ $((${#current_line} + ${#port} + 1)) -lt $width ]; then
-      current_line+=", $port"
-    else
-      echo "$current_line"
-      current_line="$port"
-    fi
-  done
-  [ -n "$current_line" ] && echo "$current_line"
-
-  echo
-  read -p "Press [Enter] to exit the view." dummy
-}
-
 # Function to open a port
 open_port() {
   clear_screen
@@ -91,7 +63,7 @@ while true; do
   read -p "Choose an option: " choice
 
   case "$choice" in
-    V|v) view_open_ports ;;
+    V|v) bash ./portsecurity_view.sh ;;
     O|o) open_port ;;
     C|c) close_port ;;
     Z|z) exit 0 ;;
