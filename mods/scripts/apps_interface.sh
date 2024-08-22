f3eefa338b418081939d62364d3bb57688528543
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2024-08-22 15:01:00 -0400

    Update apps_interface.sh

--- mods/scripts/apps_interface.sh
+++ mods/scripts/apps_interface.sh
@@ -17,6 +17,31 @@ check_deployment_status() {
     fi
 }
 
+# Function: parse_and_store_defaults
+parse_and_store_defaults() {
+    local app_name=$1
+    local app_path="/pg/apps/${app_name}"
+    local config_path="/pg/config/${app_name}.cfg"
+
+    # Check if the config file exists, create it if not
+    [[ ! -f "$config_path" ]] && touch "$config_path"
+
+    # Read through the app script for lines starting with "#####"
+    while IFS= read -r line; do
+        if [[ "$line" =~ ^##### ]]; then
+            # Remove leading "##### " and extract the key and value
+            local trimmed_line=$(echo "$line" | sed 's/^##### //')
+            local key=$(echo "$trimmed_line" | cut -d':' -f1 | tr '[:upper:]' '[:lower:]' | tr ' ' '_')
+            local value=$(echo "$trimmed_line" | cut -d':' -f2 | xargs)
+
+            # Check if the key already exists in the config file, add it if not
+            if ! grep -q "^$key=" "$config_path"; then
+                echo "$key=$value" >> "$config_path"
+            fi
+        fi
+    done < "$app_path"
+}
+
 # Function: execute_dynamic_menu
 execute_dynamic_menu() {
     local selected_option=$1
@@ -47,25 +72,14 @@ execute_dynamic_menu() {
 
 # Main Interface
 apps_interface() {
+    parse_and_store_defaults
+    
     local app_name=$1
     local config_path="/pg/config/${app_name}.cfg"
     local app_path="/pg/apps/${app_name}"
     local dynamic_menu_items=()
     local dynamic_menu_count=1
-
-    # Check if the configuration file exists; if not, create a default one
-    if [[ ! -f "$config_path" ]]; then
-        echo "Configuration file not found. Creating a default configuration at $config_path."
-        cat <<EOL > "$config_path"
-app_name=$app_name
-media_path="/pg/media"
-port_number=32400
-time_zone="America/New_York"
-appdata_path="/pg/appdata/plex"
-plex_token="null"
-EOL
-    fi
-
+    
     # Parse the app script for dynamic menu items
     while IFS= read -r line; do
         if [[ "$line" =~ ^####\  ]]; then
