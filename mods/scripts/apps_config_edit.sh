cb321ba8a16689b2c3533be01913884597f16003
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2024-08-22 08:37:19 -0400

    Update apps_config_edit.sh

--- mods/scripts/apps_config_edit.sh
+++ mods/scripts/apps_config_edit.sh
@@ -8,25 +8,53 @@ NC="\033[0m" # No color
 app_name=$1
 config_path=$2
 
+# Function to wrap text at 80 characters
+wrap_text() {
+    local text="$1"
+    local wrapped_text=""
+    local line=""
+
+    for word in $text; do
+        if [[ ${#line} + ${#word} + 1 -gt 80 ]]; then
+            wrapped_text+="$line\n"
+            line="$word "
+        else
+            line+="$word "
+        fi
+    done
+    wrapped_text+="$line"
+    echo -e "$wrapped_text"
+}
+
 clear
 edit_code=$(printf "%04d" $((RANDOM % 10000)))
-echo -e "${RED}Warning: This is an advanced option.${NC}"
-echo "Visit https://plexguide.com/wiki/not-generated-yet for more information."
+wrap_text "${RED}Warning: This is an advanced option.${NC}"
+wrap_text "Visit https://plexguide.com/wiki/not-generated-yet for more information."
 echo ""
-echo "If the container is running, it will be stopped/killed and removed due to the changes made."
-echo "You will need to redeploy the container manually."
-echo "Once changes are made, press CTRL+X to save and exit."
+wrap_text "If the container is running, it will be stopped/killed and removed due to the changes made."
+wrap_text "You will need to redeploy the container manually."
+wrap_text "Once changes are made, press CTRL+X to save and exit."
 echo ""
+
 while true; do
-    read -p "$(echo -e "Do you want to proceed? Type [${RED}${edit_code}${NC}] to proceed or [${GREEN}no${NC}] to cancel: ")" edit_choice
+    read -p "$(wrap_text "Do you want to proceed? Type [${RED}${edit_code}${NC}] to proceed or [${GREEN}no${NC}] to cancel: ")" edit_choice
     if [[ "$edit_choice" == "$edit_code" ]]; then
         nano "$config_path"
-        bash /pg/apps/stop_and_remove_app.sh "$app_name"
+
+        # Stop and remove the Docker container if running
+        docker ps --filter "name=^/${app_name}$" --format "{{.Names}}" &> /dev/null
+        if [[ $? -eq 0 ]]; then
+            wrap_text "Stopping and removing the existing container for $app_name ..."
+            docker stop "$app_name" && docker rm "$app_name"
+        else
+            wrap_text "Container $app_name is not running."
+        fi
+
         break
     elif [[ "${edit_choice,,}" == "no" ]]; then
-        echo "Operation cancelled."
+        wrap_text "Operation cancelled."
         break
     else
-        echo -e "${RED}Invalid response.${NC} Please type [${RED}${edit_code}${NC}] or [${GREEN}no${NC}]."
+        wrap_text "${RED}Invalid response.${NC} Please type [${RED}${edit_code}${NC}] or [${GREEN}no${NC}]."
     fi
 done
