f962985672658635bc790802da93129fe382b44f
Admin9705 <9705@duck.com>
2024-08-24 12:07:25 -0400

    update

--- mods/scripts/apps_support.sh
+++ mods/scripts/apps_support.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 # Function to verify if the Docker container is running
+appsourcing() {
+     source "/pg/config/${app_name}.cfg"
+    source /pg/apps/${app_name}/${app_name}.functions 2>/dev/null
+}
+
+# Function to verify if the Docker container is running
 appverify() {
     local app_name="$1"
     docker ps | grep "$app_name"
 }
-
+ 
 # Function to source configuration from the config file
 configsource() {
     local app_name="$1"
