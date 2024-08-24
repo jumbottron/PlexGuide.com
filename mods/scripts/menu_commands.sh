55740e074ab1d5a915e6c64948ea09a584bd5cdc
Admin9705 <9705@duck.com>
2024-08-24 14:58:30 -0400

    commands update

--- mods/scripts/menu_commands.sh
+++ mods/scripts/menu_commands.sh
@@ -2,6 +2,20 @@
 
 # Function to check and create symbolic links for plexguide and pg commands
 check_and_create_commands() {
+    
+    # List of files to remove
+    files=(
+        "/usr/local/bin/plexguide"
+        "/usr/local/bin/pg"
+        "/usr/local/bin/pgalpha"
+    )
+
+    # Loop through each file and remove it
+    for file in "${files[@]}"; do
+        rm -rf "$file"
+    done
+
+    # Create PlexGuide Commands
     if [[ ! -f "/usr/local/bin/plexguide" ]]; then
         sudo ln -s /pg/scripts/menu.sh /usr/local/bin/plexguide
         sudo chmod +x /usr/local/bin/plexguide
@@ -13,8 +27,9 @@ check_and_create_commands() {
     fi
 
     if [[ ! -f "/usr/local/bin/pgalpha" ]]; then
-        sudo ln -s /pg/scripts/menu_reinstall.sh /usr/local/bin/pgalpha
+        sudo ln -s /pg/scripts/menu_reinstall.sh /usr/local/bin/pgalpha        
         sudo chmod +x /usr/local/bin/pgalpha
+        plexguide
     fi
 
     # Apply chmod +x to every file in /pg/scripts/ and /pg/apps/
