9593380e7ff3f3c077a47a93ed0a1620b31c8006
Admin9705 <9705@duck.com>
2024-08-24 13:44:59 -0400

    update

--- mods/scripts/apps_kill_remove.sh
+++ mods/scripts/apps_kill_remove.sh
@@ -1,16 +1,43 @@
 #!/bin/bash
 
+# ANSI color codes
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+NC="\033[0m" # No color
+
 app_name=$1
 
 # Function: stop_and_remove_app
 stop_and_remove_app() {
-    docker ps --filter "name=^/${app_name}$" --format "{{.Names}}" &> /dev/null
-    if [[ $? -eq 0 ]]; then
-        echo "Stopping and removing the existing container for $app_name ..."
-        docker stop "$app_name" && docker rm "$app_name"
-    else
-        echo "Container $app_name is not running."
-    fi
+    while true; do
+        clear
+        echo "This action will stop and remove the Docker container for $app_name."
+        echo "Your appdata will not be lost."
+        echo ""
+        
+        # Generate a random 4-digit code
+        pin_code=$(printf "%04d" $((RANDOM % 10000)))
+        
+        # Prompt the user to confirm with the generated pin code or to cancel with 'Z'
+        echo -e "To proceed, type [${RED}${pin_code}${NC}] to proceed or [${GREEN}Z${NC}] to cancel: "
+        
+        read -p "" user_input
+        
+        if [[ "$user_input" == "$pin_code" ]]; then
+            # If the user enters the correct pin code, proceed to stop and remove the container
+            echo "Stopping and removing the existing container for $app_name ..."
+            docker stop "$app_name" && docker rm "$app_name"
+            break
+        elif [[ "${user_input,,}" == "z" ]]; then
+            # If the user types 'Z' or 'z', cancel the operation
+            echo "Operation cancelled."
+            break
+        else
+            # If the input is invalid, clear the screen and repeat the prompt
+            clear
+            echo -e "${RED}Invalid input. Please enter the correct pin code or [Z] to cancel.${NC}"
+        fi
+    done
 }
 
 # Run the stop and remove function
