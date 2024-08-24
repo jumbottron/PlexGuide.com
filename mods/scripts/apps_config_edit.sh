43e85e427165f5d1cfef6d8318ab16d759fefdc8
Admin9705 <9705@duck.com>
2024-08-24 13:27:02 -0400

    update

--- mods/scripts/apps_config_edit.sh
+++ mods/scripts/apps_config_edit.sh
@@ -12,26 +12,37 @@ clear
 edit_code=$(printf "%04d" $((RANDOM % 10000)))
 
 echo -e "${RED}Warning: This is an advanced option.${NC}"
-echo "Visit https://plexguide.com/wiki/not-generated-yet for more information."
+echo "Visit https://plexguide.com/wiki/link-not-set for more information."
 echo ""
-echo "If the container is running, it will be stopped/killed and removed due to the"
-echo "changes made. You will need to redeploy the container manually. Once changes"
-echo "are made, press CTRL+X to save and exit."
+echo "This will allow you to modify the current config file."
+echo "The Docker container will be stopped and removed if running."
+echo "You must deploy the Docker container again to accept your changes."
 echo ""
 echo -e "Do you want to proceed? Type [${RED}${edit_code}${NC}] to proceed or [${GREEN}no${NC}] to cancel: "
 
 while true; do
     read -p "" edit_choice
     if [[ "$edit_choice" == "$edit_code" ]]; then
+        # Capture file's modification time before editing
+        before_edit=$(stat -c %Y "$config_path")
+
         nano "$config_path"
 
-        # Stop and remove the Docker container if running
-        docker ps --filter "name=^/${app_name}$" --format "{{.Names}}" &> /dev/null
-        if [[ $? -eq 0 ]]; then
-            echo "Stopping and removing the existing container for $app_name ..."
-            docker stop "$app_name" && docker rm "$app_name"
+        # Capture file's modification time after editing
+        after_edit=$(stat -c %Y "$config_path")
+
+        # Check if the config file was modified
+        if [[ "$before_edit" != "$after_edit" ]]; then
+            # Stop and remove the Docker container if running
+            docker ps --filter "name=^/${app_name}$" --format "{{.Names}}" &> /dev/null
+            if [[ $? -eq 0 ]]; then
+                echo "Stopping and removing the existing container for $app_name ..."
+                docker stop "$app_name" && docker rm "$app_name"
+            else
+                echo "Container $app_name is not running."
+            fi
         else
-            echo "Container $app_name is not running."
+            echo "No changes detected in the config file. No need to stop the container."
         fi
 
         break
