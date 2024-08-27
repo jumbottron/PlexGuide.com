8471da81772707a50230db63cd90f792b6ec6b8b
Admin9705 <9705@duck.com>
2024-08-27 14:55:00 -0400

    updates

--- mods/scripts/menu_commands.sh
+++ mods/scripts/menu_commands.sh
@@ -20,21 +20,25 @@ check_and_create_commands() {
     if [[ ! -f "/usr/local/bin/plexguide" ]]; then
         sudo ln -s /pg/scripts/menu.sh /usr/local/bin/plexguide
         sudo chmod +x /usr/local/bin/plexguide
+        bash /pg/scripts/menu_exit.sh
     fi
 
     if [[ ! -f "/usr/local/bin/pg" ]]; then
         sudo ln -s /pg/scripts/menu.sh /usr/local/bin/pg
         sudo chmod +x /usr/local/bin/pg
+        bash /pg/scripts/menu_exit.sh
     fi
 
     if [[ ! -f "/usr/local/bin/pgalpha" ]]; then
         sudo ln -s /pg/scripts/menu_reinstall.sh /usr/local/bin/pgalpha        
         sudo chmod +x /usr/local/bin/pgalpha
+        bash /pg/scripts/menu_exit.sh
     fi
 
     if [[ ! -f "/usr/local/bin/pgbeta" ]]; then
         sudo ln -s /pg/scripts/install_beta.sh /usr/local/bin/pgbeta       
         sudo chmod +x /usr/local/bin/pgbeta
+        bash /pg/scripts/menu_exit.sh
     fi
 
     # Apply chmod +x and set ownership to user with UID and GID 1000 for every file in /pg/scripts/ and /pg/apps/
